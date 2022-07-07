30dac8838ff54f715d838f9c1b105de6fe8b0d7c
BGOPC <bb8933400@gmail.com>
2022-07-07 10:20:33 +0430

    Making Parser Lv.1

---
+++ mainclasses/Parser.py
@@ -0,0 +1,12 @@
+from .utils.Nodes.BinOpNode import BinOpNode
+from .utils.Nodes.NumberNode import NumberNode
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.tok_idx = -1
+        self.advance()
+    def advance(self):
+        self.tok_idx += 1
+        if self.tok_idx > len(self.tokens):
+            pass
