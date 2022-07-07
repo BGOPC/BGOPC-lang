7143aa1d17deba896fdff879aa7f4aba9bd58da6
BGOPC <bb8933400@gmail.com>
2022-07-07 10:17:24 +0430

    Basic Nodes Added

---
+++ mainclasses/utils/Nodes/NumberNode.py
@@ -0,0 +1,8 @@
+from ....mainclasses.token import Token, enums
+class NumberNode:
+    def __init__(self, tok: Token):
+        self.tok = tok
+
+    def __repr__(self) -> str:
+        return f"{self.tok}"
+
